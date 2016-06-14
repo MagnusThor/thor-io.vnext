b206096ee7c01b4366d47509ae5c62b51b47128e
Magnus Thor <magnus@xsockets.net>
2016-06-14 16:33:47 +0200

    Removed unused files and renamed folders

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require("express");
 app = express();
-var rt = require("./controller/thor-io.js");
+var rt = require("./src/thor-io.js");
 
-//var foo = new rt.ThorIO.Generic();
-
- var controllers = [{
-    alias:"generic",
+var controllers = [{
+    alias: "generic",
     instance: rt.Generic
- }];
+}];
 
 
 var thorIO = new rt.ThorIO.Engine(controllers);
@@ -18,7 +16,7 @@ var expressWs = require("express-ws")(app);
 app.use('/client', express.static('client'));
 
 app.ws("/", function(ws, req) {
-   thorIO.addConnection(ws);
+    thorIO.addConnection(ws);
 });
 
 
