e33426fc797c57a0e73380244872052d59b189c5
Magnus Thor <magnus.thor74@gmail.con>
2016-09-13 15:49:18 +0200

    ThorIO.Client.Recorder added , now record MediaStreams

--- src/client/thor-io.client.js
+++ src/client/thor-io.client.js
@@ -42,6 +42,56 @@ var ThorIO;
             return WebRTCConnection;
         }());
         Client.WebRTCConnection = WebRTCConnection;
+        var Recorder = (function () {
+            function Recorder(stream, mimeType, ignoreMutedMedia) {
+                var _this = this;
+                this.stream = stream;
+                this.mimeType = mimeType;
+                this.ignoreMutedMedia = ignoreMutedMedia;
+                this.recorder = new MediaRecorder(stream, { mimeType: mimeType, ignoreMutedMedia: ignoreMutedMedia });
+                this.recorder.onstop = function (event) {
+                    _this.handleStop(event);
+                };
+                this.recorder.ondataavailable = function (event) {
+                    _this.handleDataAvailable(event);
+                };
+            }
+            Recorder.prototype.handleStop = function (event) {
+                this.IsRecording = false;
+                var blob = new Blob(this.blobs, { type: this.mimeType });
+                this.OnRecordComplated.apply(event, [blob, URL.createObjectURL(blob)]);
+            };
+            Recorder.prototype.OnRecordComplated = function (blob, blobUrl) { };
+            Recorder.prototype.handleDataAvailable = function (event) {
+                if (event.data.size > 0) {
+                    this.blobs.push(event.data);
+                }
+            };
+            Recorder.prototype.IsTypeSupported = function (type) {
+                throw "not yet implemented";
+            };
+            Recorder.prototype.GetStats = function () {
+                return {
+                    videoBitsPerSecond: this.recorder.videoBitsPerSecond,
+                    audioBitsPerSecond: this.recorder.audioBitsPerSecond
+                };
+            };
+            Recorder.prototype.Stop = function () {
+                this.recorder.stop();
+            };
+            Recorder.prototype.Start = function (ms) {
+                this.blobs = new Array();
+                if (this.IsRecording) {
+                    this.Stop();
+                    return;
+                }
+                this.blobs.length = 0;
+                this.IsRecording = true;
+                this.recorder.start(ms || 100);
+            };
+            return Recorder;
+        }());
+        Client.Recorder = Recorder;
         var DataChannel = (function () {
             function DataChannel(name, listeners) {
                 this.listeners = listeners || new Array();
@@ -356,7 +406,6 @@ var ThorIO;
             function Factory(url, controllers, params) {
                 var _this = this;
                 this.url = url;
-                this.proxys = new Array();
                 this.ws = new WebSocket(url + this.toQuery(params || {}));
                 controllers.forEach(function (alias) {
                     _this.proxys.push(new Proxy(alias, _this.ws));
--- src/client/thor-io.client.ts
+++ src/client/thor-io.client.ts
@@ -1,4 +1,5 @@
 
+declare var MediaRecorder: any;
 namespace ThorIO.Client {
     export class Message {
         T: string;
@@ -35,6 +36,64 @@ namespace ThorIO.Client {
             this.rtcPeerConnection = rtcPeerConnection;
             this.streams = new Array<any>();
         }
+    }
+
+
+
+    export class Recorder {
+        private recorder: MediaRecorder;
+        private blobs: Array<any>;
+        public IsRecording: boolean;
+        constructor(private stream: MediaStream, private mimeType: string, private ignoreMutedMedia) {
+            this.recorder = new MediaRecorder(stream,
+                { mimeType: mimeType, ignoreMutedMedia: ignoreMutedMedia }
+            );
+            this.recorder.onstop = (event) => {
+                this.handleStop(event);
+            }
+            this.recorder.ondataavailable = (event) => {
+                this.handleDataAvailable(event)
+            };
+        }
+
+        private handleStop(event: any) {
+            this.IsRecording = false;
+            let blob = new Blob(this.blobs, { type: this.mimeType });
+            this.OnRecordComplated.apply(event, [blob, URL.createObjectURL(blob)]);
+        }
+
+        public OnRecordComplated(blob: any, blobUrl: string) { }
+
+        private handleDataAvailable(event: any) {
+            if (event.data.size > 0) {
+                this.blobs.push(event.data);
+            }
+        }
+      
+        IsTypeSupported(type:string){
+                throw "not yet implemented";
+        }
+        GetStats():any{
+            return {
+                videoBitsPerSecond: this.recorder.videoBitsPerSecond,
+                audioBitsPerSecond: this.recorder.audioBitsPerSecond
+            }
+        }
+        Stop() {
+            this.recorder.stop();
+        }
+        Start(ms: number) {
+            this.blobs = new Array<any>();
+            if (this.IsRecording) {
+                this.Stop();
+                return;
+            }
+            this.blobs.length = 0;
+            this.IsRecording = true;
+
+            this.recorder.start(ms || 100);
+        }
+
     }
 
     export class DataChannel {
@@ -377,8 +436,6 @@ namespace ThorIO.Client {
         private proxys: Array<ThorIO.Client.Proxy>;
         public IsConnected: boolean;
         constructor(private url: string, controllers: Array<string>, params?: any) {
-
-            this.proxys = new Array<ThorIO.Client.Proxy>();
             this.ws = new WebSocket(url + this.toQuery(params || {}));
             controllers.forEach(alias => {
                 this.proxys.push(
